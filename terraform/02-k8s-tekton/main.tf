24da779697c3532ce2311850ef8cd661046ca6b6
Stanimir Ivanov <stanimr@protonmail.com>
2026-08-05 12:47:17 +0200

    [#3] Fix helm provider

--- terraform/02-k8s-tekton/main.tf
+++ terraform/02-k8s-tekton/main.tf
@@ -36,11 +36,11 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = local.kubeconfig.clusters[0].cluster.server
     cluster_ca_certificate = base64decode(local.kubeconfig.clusters[0].cluster["certificate-authority-data"])
 
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "oci"
       args        = ["ce", "cluster", "generate-token", "--cluster-id", var.cluster_id, "--region", var.region]
